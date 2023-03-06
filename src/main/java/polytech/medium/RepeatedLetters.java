a0ef5979a58fb630f8f46c43690f6533845e3fb4
Stepan-Zazyan <stepanzazyan@yandex.ru>
2023-03-06 22:46:25 +0300

    delete Repeated Letters - O(n) complexity using stack [Easy & Works]

--- src/main/java/polytech/medium/RepeatedLetters.java
+++ src/main/java/polytech/medium/RepeatedLetters.java
@@ -1,40 +1,23 @@
 package polytech.medium;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class RepeatedLetters {
+    public static void main(String[] args) {
+    }
 
     public String deleteRepeatedLetters(String str) {
         List<String> list = new ArrayList<>(List.of(str.split("")));
         Stack<String> stack = new Stack<>();
-        Stack<String> st = new Stack<>();
         for (String s : list) {
-            stack.push(s);
-        }
-        while (!stack.empty()) {
-            if (!st.empty()) {
-                while (!st.empty()) {
-                    if (!stack.empty() && Objects.equals(st.peek(), stack.peek())) {
-                        st.pop();
-                        stack.pop();
-                    } else {
-                        break;
-                    }
-                }
-            }
-            if (!stack.empty()) {
-                String s = stack.pop();
-                if (!stack.empty() && Objects.equals(s, stack.peek())) {
-                    stack.pop();
-                } else {
-                    st.push(s);
-                }
+            if (!stack.empty() && Objects.equals(stack.peek(), s)) {
+                stack.pop();
+            } else {
+                stack.push(s);
             }
         }
-        String result = "";
-        while (!st.empty()) {
-            result += st.pop();
-        }
-        return result;
+        return stack.stream().map(String::valueOf)
+                .collect(Collectors.joining(""));
     }
 }
